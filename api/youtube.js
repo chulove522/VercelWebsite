84af5c0fcf368c656d5c09ef314a9132a5f127ca
yz <chulove522@hotmail.com.tw>
2025-04-30 03:32:15 +0800

    yt code
    yt code

---
+++ api/youtube.js
@@ -0,0 +1,13 @@
+// /api/youtube.js
+export default async function handler(req, res) {
+  const API_KEY = process.env.YOUTUBE_API_KEY;
+  const CHANNEL_ID = 'UCB2VtFEb4Qslt3En-WOUqXQ';
+  const maxResults = 3;
+
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${maxResults}&type=video`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
